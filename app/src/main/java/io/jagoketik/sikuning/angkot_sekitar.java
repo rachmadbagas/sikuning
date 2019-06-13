bac23137de0e02758da6df9d9b949fefd332a437
JohnKontolatos <dodeardi@gmail.com>
2019-06-13 13:12:13 +0700

    ayo dikerjakan

---
+++ app/src/main/java/io/jagoketik/sikuning/angkot_sekitar.java
@@ -0,0 +1,23 @@
+package io.jagoketik.sikuning;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class angkot_sekitar extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_angkot_sekitar, container, false);
+    }
+
+
+}
