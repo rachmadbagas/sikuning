41e0e86e831ec23bd4684e3c0647ee733a773e73
JohnKontolatos <dodeardi@gmail.com>
2019-06-17 12:41:09 +0700

    scrollllll beybeh

---
+++ app/src/main/java/io/jagoketik/sikuning/info_angkot.java
@@ -0,0 +1,13 @@
+package io.jagoketik.sikuning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class info_angkot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_angkot);
+    }
+}
